f365a4741a847e3634d13236f2f22324b0c4146f
Dev Joshi <dev@BlackBox.localdomain>
2022-06-12 19:51:17 +0530

    Initail page layout added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 
 const Home: NextPage = () => {
-  return <div className="text-4xl text-red-500">hello</div>;
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text text-2xl p-2">Which is more pointy than two?</div>
+      <div className="flex max-w-2xl justify-between p-8 items-center">
+        <div className="flex flex-col">
+          <div className="w-36 h-36 bg-slate-400">first</div>
+          <button className="border rounded-full m-2">Choose</button>
+        </div>
+        <div className="p-8 text text-xl">vs</div>
+        <div className="flex flex-col">
+          <div className="w-36 h-36 bg-slate-400">second</div>
+          <button className="border rounded-full m-2">Choose</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
